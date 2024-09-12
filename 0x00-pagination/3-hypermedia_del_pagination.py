584d16ffb67497dd9ecdc09b8b5dc24c316ce1c3
abdallhfattah <abdallh.fattah33@gmail.com>
2024-09-12 09:30:07 +0300

    hyper media after deletion

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0"""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Retrieves info about a page from a given index and with a
+        specified size.
+        """
+        data = self.indexed_dataset()
+        assert index is not None and index >= 0 and index <= max(data.keys())
+
+        next_index = None
+        page_data = []
+        c_ps = page_size
+
+        for i, item in data.items():
+            if c_ps != 0:
+                page_data.append(item)
+                c_ps -= 1
+            else:
+                next_index = i
+                break
+        page_info = {
+            "index": index,
+            "next_index": next_index,
+            "page_size": len(page_data),
+            "data": page_data,
+        }
+        return page_info
