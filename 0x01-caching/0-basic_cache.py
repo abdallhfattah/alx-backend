d2b3c26f6e4139bd89611450b5bca753232ad3e1
abdallhfattah <abdallh.fattah33@gmail.com>
2024-09-23 09:26:09 +0300

    basic cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
-'''
+"""
   basic caching
-'''
+"""
 
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    '''
+    """
     A class `BasicCache` that inherits from `BaseCaching`
        and is a caching system
-    '''
+    """
 
     def get(self, key):
-        '''Get an item by key
-        '''
+        """Get an item by key"""
         self.cache_data.get(key, None)
 
     def put(self, key, item):
-        '''
+        """
         Add an item in the cache
-        '''
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
