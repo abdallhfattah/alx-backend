08caeaaede5c975a00b5ebf663c14e9f951a01a4
abdallhfattah <abdallh.fattah33@gmail.com>
2024-09-23 09:30:16 +0300

    Fifo caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,10 +3,8 @@
   basic caching
 """
 
-from typing import override
 
 from queue import Queue
-
 from base_caching import BaseCaching
 
 
@@ -21,12 +19,10 @@ class FIFOCache(BaseCaching):
         # FIFO mimic
         self.queue = Queue(maxlen=super.MAX_ITEMS)
 
-    @override
     def get(self, key):
         """Get an item by key"""
         return self.cache_data.get(key, None)
 
-    @override
     def put(self, key, item):
         """
         Add an item in the cache
